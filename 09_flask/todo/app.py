e3ff450c92f3fd93d334f4ba18fffffa64c6ec48
yuuki.fujimoto3 <fujimoto.yuki@marble-corp.co.jp>
2025-12-05 10:48:43 +0900

    TODOリストの課題完了

--- 09_flask/todo/app.py
+++ 09_flask/todo/app.py
@@ -29,7 +29,9 @@ def index():
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    # 課題で実装します。
+    todos = load_todos()
+    todos.pop(todo_id)
+    save_todos(todos)
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
